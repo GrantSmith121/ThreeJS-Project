95852e836f46b1b881fb08d8059584305d83df42
Grant Smith <70243470+GrantSmith121@users.noreply.github.com>
2024-04-07 08:56:16 -0400

    Materials on plane (#2)
    * Replacing sample texture with new albedo texture
    
    * Plane material has normal map
    
    Changed the material type to "MeshPhrongMaterial" and added ambient light to view the texture
    
    * Set up orbit controls for easier debugging
    
    * Normal Map now receives lighting

--- main.js
+++ main.js
@@ -1,31 +1,60 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 let camera, scene, renderer;
 
 init();
-animate();
+//animate();
 
 function init() {
 
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
     camera.position.z = 10;
 
+
     scene = new THREE.Scene();
-       
+    
     const geometry = new THREE.PlaneGeometry( 10, 10 ); // ensure aspect ratio matches image
-    const material = new THREE.MeshBasicMaterial( { map:THREE.ImageUtils.loadTexture('uv_grid_opengl.jpg') } );
+    const loader = new THREE.TextureLoader(); // shorthand for loading textures
+    const texture = loader.load('/Moss002_1K-JPG_Color.jpg');
+    const normalMap = loader.load('/Moss002_1K-JPG_NormalDX.jpg');
+
+    const material = new THREE.MeshLambertMaterial({
+        map: texture,
+        normalMap: normalMap,
+    });
+
+    normalMap.flipY = false;
+
+    // debugging light to view material
+    const ambientLight = new THREE.AmbientLight( 0x404040 );
+
+    const light = new THREE.SpotLight( 0xffffff, 20);
+    light.position.set( 0, 2, 2 );
+    const lightHelper = new THREE.SpotLightHelper(light, 2);
+    light.castShadow = true;
 
     const mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
+    scene.add( ambientLight );
+    scene.add( light );
+    scene.add( lightHelper );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+
+    // lets me see the setup from different angles
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.update();
 }
 
-// This is run on every frame >:>
-function animate() {
-    requestAnimationFrame( animate );
+//renderer.render( scene, camera );
+
+function render() {
     renderer.render( scene, camera );
+    requestAnimationFrame(render);
 }
+
+render();
