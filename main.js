d5c280411cb7d14724a1bcc616d3eae5f7ba4762
GrrantSmith121 <gss1@iu.edu>
2024-04-07 14:24:03 -0400

    Light now follows exact mouse position

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ import * as THREE from 'three';
 
 let camera, scene, renderer;
 
-const light = new THREE.SpotLight( 0xffffff, 20);
-let clientX = 0;
-let clientY = 0;
+
+let clientX = window.innerWidth / 2;
+let clientY = window.innerHeight / 2;
 
 init();
 //animate();
@@ -33,8 +33,8 @@ function init() {
     // debugging light to view material
     const ambientLight = new THREE.AmbientLight( 0x404040 );
 
-    //const light = new THREE.SpotLight( 0xffffff, 20);
-    //light.position.set( 0, 2, 2 );
+    const light = new THREE.SpotLight( 0xffffff, 100);
+    light.position.set( clientX, clientY, 5 );
     const lightHelper = new THREE.SpotLightHelper(light, 2);
     light.castShadow = true;
 
@@ -59,15 +59,11 @@ function init() {
           clientX,
           clientY
         } = event
-        console.log(clientX, clientY)
-        light.position.set( clientX / 200, clientY / 200, 2 );
+        light.position.set( ((clientX) - (window.innerWidth / 2)) / 200, ((clientY) - (window.innerHeight / 2)) / -200, 5 );
+        //console.log(light.position);
       }
     
 }
-
-
-
-//renderer.render( scene, camera );
 
 function render() {
     renderer.render( scene, camera );
